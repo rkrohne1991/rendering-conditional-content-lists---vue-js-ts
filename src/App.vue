e9c713d606333df85a98cff0cb0dc4ca7d1d1e4a
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-06 12:41:54 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>Vue Course Goals</h1>
+  </header>
+  <section id="user-goals">
+    <h2>My course goals</h2>
+    <input type="text" />
+    <button>Add Goal</button>
+    <p>No goals have been added yet - please start adding some!</p>
+    <ul>
+      <li>Goal</li>
+    </ul>
+  </section>
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return { goals: [] };
+  },
+});
+</script>
