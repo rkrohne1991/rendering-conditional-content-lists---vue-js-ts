566cd7918e43bd818bb3aac14450426d425082d1
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-06 14:08:54 +0200

    Rendering content conditionally, using  v-if, v-else and v-else-if, removing list item, lists and keys

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,29 @@
   </header>
   <section id="user-goals">
     <h2>My course goals</h2>
-    <input type="text" />
-    <button>Add Goal</button>
-    <p>No goals have been added yet - please start adding some!</p>
-    <ul>
-      <li>Goal</li>
+    <input type="text" v-model="enteredGoalValue" />
+    <button @click="addGoal">Add Goal</button>
+    <p v-if="goals.length === 0">
+      No goals have been added yet - please start adding some!
+    </p>
+    <ul v-else>
+      <li
+        v-for="(goal, index) in goals"
+        :key="index"
+        @click="removeGoal(index)"
+      >
+        <p>{{ goal }} - {{ index }}</p>
+        <input type="text" @click.stop />
+      </li>
     </ul>
+    <!-- <ul>
+      <li v-for="num in 10" :key="num">{{ num }}</li>
+    </ul> -->
+    <!-- <ul>
+      <li v-for="(value, key, index) in { name: 'Max', age: 31 }" :key="index">
+        {{ key }}: {{ value }}
+      </li>
+    </ul> -->
   </section>
 </template>
 
@@ -22,7 +39,15 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   data() {
-    return { goals: [] };
+    return { enteredGoalValue: '' as string, goals: [] as Array<string> };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(idx: number) {
+      this.goals.splice(idx, 1);
+    },
   },
 });
 </script>
